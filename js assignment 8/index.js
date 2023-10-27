fdd76e1ddb1f833ed785f5ee74f48caa86836fe1
syedjamilhussain <123335989+syedjamilhussain@users.noreply.github.com>
2023-10-27 07:32:14 +0500

    Add files via upload

---
+++ js assignment 8/index.js
@@ -0,0 +1,183 @@
+// CHAPTER NUMBER  38 TO 42    ...
+
+// QUESTION NUMBER 1 ... 
+
+//   function power(a,b){
+//     return Math.pow(a,b);
+//   }
+
+//    var power = power(5,3);
+//    document.write(power);
+
+// QUESTION NUMBER  2 ....
+
+// function leapYear(year){
+//     if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+//       return   "this is leap year ";
+//     } else{
+//         return "this is not leap year ";
+//     }
+    
+// }
+
+//  var year = leapYear(2004);
+// document.write(year);
+
+// QUESTION NUMBER 3 ...
+    
+
+// function triangleArea(a,b,c){
+//     var a ;
+//     var b;
+//     var c ;
+//     s = a + b + c / 2;
+//       area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+//       return "this is the area of tringle" + area 
+// }
+
+
+// var triangleArea = triangleArea(20,30,30);
+// document.write(triangleArea);
+
+
+//  QUESTION NUMBER 4 ;
+// function findPercentage(mark1,mark2,mark3){
+//   function a(){
+     
+//         var totalMarks = 550;
+//         let percentage = (mark1 / totalMarks) * 100;
+//          return percentage;
+
+//     }
+//     function b(){
+//         var totalMarks = 660;
+//         let percentage1 = (mark2 / totalMarks) * 100;
+//         return percentage1;
+//     }
+//     function c(){
+//         var totalMarks = 1100;
+//         let percentage2 = (mark3 / totalMarks) * 100;
+//         return  percentage2;
+//     }
+//     return "first student " + a() +"%" + "<br>" +"second student " + b() + "%"+ "<br>" + "third student " + c() + "%";
+// }
+//  var myPercentage = findPercentage(400,600,800);
+//  document.write(myPercentage);
+
+//  QUESTION NUMBER 5 ..... 
+ 
+// function customIndexOf(jam, char) {
+//     for (let i = 0; i < jam.length; i++) {
+//         if (jam[i] === char) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+
+// let jam = "jamil hussain";
+// let index = customIndexOf(jam, 'h');
+// console.log(index); 
+
+// QUESTION NUMBER 6...
+
+// function remove(sentence){
+//     return sentence.replace(/[aeiouAEIOU]/g, '');
+
+
+// }
+//  var sentence = "hello my name is syed jamil hussain";
+//   var remove = remove(sentence);
+//   document.write(remove);
+
+
+// QUESTION NUMBER 7 ...
+
+// SMJ NAE AYIA 
+
+// QUESTION NUMBER 8...
+
+
+
+// a 
+// function convertKmIntoMeter(km){
+//      var km;
+//      var convert = km * 1000;
+//      return convert;
+
+// }
+// document.write(convertKmIntoMeter(500) + "<br>");
+
+// b 
+// function convertKmIntoFeet(km){
+//     var km;
+//     var convert = km * 3280.84;
+//     return convert;
+
+// }
+// document.write(convertKmIntoFeet(500) + "<br>");
+
+// c 
+
+// function convertKmIntoInches(km){
+//     var km;
+//     var convert = km *  3280.84 * 12;
+//     return convert;
+
+// }
+// document.write(convertKmIntoInches(500) + "<br>");
+
+// d 
+
+// function convertKmIntoCentimeters(km){
+//         var km;
+//        var convert = km *  100000;
+//        return convert;
+    
+//      }
+//      document.write(convertKmIntoCentimeters(500) + "<br>");
+
+// QUESTION NUMBER 9..
+
+// function overTimePay(hours){
+//     var regularHours = 40;
+//     var overTimeRate = 1200;
+    
+
+//     if(hours > regularHours){
+//         var totalHours = hours - regularHours;
+//         return hours * totalHours;
+
+//     } else{
+//         return 0, "no over time.."
+//     }
+
+// }
+// var hours = 45;
+//  var laboursOverTime = overTimePay(hours);
+//  document.write(laboursOverTime);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
